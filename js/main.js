e77997de1e55dd4a0c8d9ea2831726fba6a0c26c
Vu Dat <vudat42@gmail.com>
2024-02-11 20:57:18 +0700

    integrate win logic

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-import { TURN } from "./constants.js";
-import { getCellElementAtIdx, getCellElementList, getCurrentTurnElement, getGameStatusElement } from "./selectors.js";
+import { CELL_VALUE, GAME_STATUS, TURN } from "./constants.js";
+import { getCellElementAtIdx, getCellElementList, getCurrentTurnElement, getGameStatusElement, getReplayGameButton } from "./selectors.js";
 import { checkGameStatus } from "./utils.js";
 
 /**
@@ -25,11 +25,6 @@ let cellValues = new Array(9).fill("");
  *
  */
 
-console.log(checkGameStatus(['X', 'O', 'O', '', 'X', '', '', 'O', 'X']));
-console.log(checkGameStatus(['O', 'O', 'O', '', 'X', '', '', 'O', 'X']));
-console.log(checkGameStatus(['O', 'X', 'O', 'O', 'X', 'O', 'X', 'O', 'X']));
-console.log(checkGameStatus(['O', 'X', 'O', 'O', 'X', 'O', 'X', '', 'X']));
-
 function toggleTurn() {
     currentTurn = currentTurn === TURN.CROSS ? TURN.CIRCLE : TURN.CROSS;
 
@@ -40,12 +35,64 @@ function toggleTurn() {
     }
 }
 
+function updateGameStatus(status) {
+    const statusElement = getGameStatusElement();
+    if (statusElement) statusElement.textContent = status;
+}
+
+function highlightWinCells(winPositions) {
+    if (!Array.isArray(winPositions) || winPositions.length !== 3) {
+        throw new Error('Invalid win positions');
+    }
+
+    for (const cellIndex of winPositions) {
+        const winCell = getCellElementAtIdx(cellIndex);
+        winCell.classList.add('win');
+    }
+}
+
+function showReplayGameButton(status) {
+    const replayGameButton = getReplayGameButton();
+    if (!replayGameButton) return;
+
+    if (status !== GAME_STATUS.PLAYING) replayGameButton.classList.add('show');
+
+    isGameEnded = true;
+}
+
 function handleCellClick(cell, index) {
-    if (cell.classList.contains(TURN.CROSS) || cell.classList.contains(TURN.CIRCLE)) return;
+    if (cell.classList.contains(TURN.CROSS) || cell.classList.contains(TURN.CIRCLE) || isGameEnded) return;
 
     cell.classList.add(currentTurn);
 
+    // update cellValues
+    cellValues[index] = currentTurn === TURN.CROSS ? CELL_VALUE.CROSS : CELL_VALUE.CIRCLE;
+
     toggleTurn();
+
+    const game = checkGameStatus(cellValues);
+    switch (game.status) {
+        case GAME_STATUS.X_WIN:
+        case GAME_STATUS.O_WIN:
+            // update game status
+            updateGameStatus(game.status);
+            // highlight win cells
+            highlightWinCells(game.winPositions);
+            // show replayGame button
+            showReplayGameButton(game.status);
+            break;
+
+        case GAME_STATUS.ENDED:
+            // update game status
+            updateGameStatus(game.status);
+            // show replayGame button
+            showReplayGameButton(game.status);
+            break;
+
+        default:
+            // update game status
+            updateGameStatus(game.status);
+    }
 }
 
 function initCellElementList() {
